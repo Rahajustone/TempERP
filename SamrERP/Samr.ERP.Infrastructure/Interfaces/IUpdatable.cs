db5afcc38a63968378b66f0a3b3857a2e3391a03
rahajustone <1992raha1@gmail.com>
2019-06-25 11:58:20 +0300

    Add ICreatable, IUpdatable and IActivable interfaces

---
+++ SamrERP/Samr.ERP.Infrastructure/Interfaces/IUpdatable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samr.ERP.Infrastructure.Interfaces
+{
+    public interface IUpdatable
+    {
+        DateTime UpdatedAt { get; set; }
+    }
+}
