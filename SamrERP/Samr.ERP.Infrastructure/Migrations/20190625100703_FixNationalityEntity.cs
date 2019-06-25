4c72df9a46bfc88d6ffabf456736d7cd804c5265
rahajustone <1992raha1@gmail.com>
2019-06-25 13:08:47 +0300

    Fix Nationality Entity

---
+++ SamrERP/Samr.ERP.Infrastructure/Migrations/20190625100703_FixNationalityEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Samr.ERP.Infrastructure.Migrations
+{
+    public partial class FixNationalityEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
