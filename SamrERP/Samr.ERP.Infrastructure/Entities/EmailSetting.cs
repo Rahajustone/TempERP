eadf7a99efa2c58c9b15c6f1779b657ab1a8333b
stein <amirashurov11@gmail.com>
2019-07-02 10:53:51 +0500

    EmailSetting

---
+++ SamrERP/Samr.ERP.Infrastructure/Entities/EmailSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Samr.ERP.Infrastructure.Entities.BaseObjects;
+using Samr.ERP.Infrastructure.Interfaces;
+
+namespace Samr.ERP.Infrastructure.Entities
+{
+    public class EmailSetting:CreatableByUserBaseObject,ICreatable,IActivable
+    {
+        public string MailServerName { get; set; }
+        public string MailServer { get; set; }
+        public int MailPort { get; set; }
+        public string SenderName { get; set; }
+        public string Sender { get; set; }
+        public string Password { get; set; }
+        public bool IsDefault { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
