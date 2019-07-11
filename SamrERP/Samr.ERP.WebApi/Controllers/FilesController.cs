eeb1544de57fdb12b81d11431fbecfec2129d433
stein <amirashurov11@gmail.com>
2019-07-11 15:23:28 +0500

    fix automapper					AutoMapper

---
+++ SamrERP/Samr.ERP.WebApi/Controllers/FilesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Samr.ERP.Core.Interfaces;
+using Samr.ERP.Core.Services;
+
+namespace Samr.ERP.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FilesController :ControllerBase
+    {
+        private readonly IFileService _fileService;
+
+        public FilesController(IFileService fileService)
+        {
+            _fileService = fileService;
+        }
+
+        [HttpGet]
+        public IActionResult GetPhoto(string path)
+        {
+            var image = System.IO.File.OpenRead(FileService.GetFullPath(path));
+            return File(image, "image/jpeg");
+        }
+
+    }
+}
