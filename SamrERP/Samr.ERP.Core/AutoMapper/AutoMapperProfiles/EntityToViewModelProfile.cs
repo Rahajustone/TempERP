34a13a10e52fd7e2082a0c9fe5e9f1732abd8234
stein <amirashurov11@gmail.com>
2019-07-18 13:32:00 +0500

    fix build

--- SamrERP/Samr.ERP.Core/AutoMapper/AutoMapperProfiles/EntityToViewModelProfile.cs
+++ SamrERP/Samr.ERP.Core/AutoMapper/AutoMapperProfiles/EntityToViewModelProfile.cs
@@ -63,7 +63,7 @@ namespace Samr.ERP.Core.AutoMapper.AutoMapperProfiles
                     src => src.MapFrom(
                         map => map.HireDate.ToShortDateString()))
                 .ForMember(dst => dst.PhotoPath,
-                    src => src.MapFrom(map => FileService.GetDownloadAction(FileService.GetResizedPath(map.PhotoPath)))
+                    src => src.MapFrom(map => FileService.GetDownloadAction(FileService.GetResizedPath(map.PhotoPath))))
                 .ReverseMap();
 
             CreateMap<Department, DepartmentViewModel>()
