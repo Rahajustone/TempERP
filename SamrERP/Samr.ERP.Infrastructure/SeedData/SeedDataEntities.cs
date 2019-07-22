3a9a2edd1d8d3a9ab77c23f509de3ea3a58194db
stein <amirashurov11@gmail.com>
2019-07-22 16:44:22 +0500

    role seed

--- SamrERP/Samr.ERP.Infrastructure/SeedData/SeedDataEntities.cs
+++ SamrERP/Samr.ERP.Infrastructure/SeedData/SeedDataEntities.cs
@@ -21,10 +21,10 @@ namespace Samr.ERP.Infrastructure.SeedData
         {
             builder.Entity<Role>()
                 .HasData(
-                    new Role { Id = new Guid("2702BCDD-104B-475D-14B5-08D70C357974"), Name = "Employee.All", Description = "Просмотр списка", Category = "Employee", NormalizedName = "EMPLOYEE.ALL"},
-                    new Role { Id = new Guid("29ECF6CE-B82F-4FC5-AE01-08D70CF9F908"), Name = "Employee.Create", Description = "Создание", Category = "Employee", NormalizedName = "EMPLOYEE.CREATE"},
-                    new Role { Id = new Guid("A8EB0E97-EAAA-4976-AE02-08D70CF9F908"), Name = "Employee.Edit", Description = "Редактирование", Category = "Employee", NormalizedName = "EMPLOYEE.EDIT"},
-                    new Role { Id = new Guid("C5DBEAAB-86A3-4400-B50A-08D70E6B40DC"), Name = "Employee.Details", Description = "Подробная информация", Category = "Employee", NormalizedName = "EMPLOYEE.DETAILS"}
+                    new Role { Id = new Guid("2702BCDD-104B-475D-14B5-08D70C357974"), Name = "Employee.All", Description = "Просмотр списка", Category = "Employee", NormalizedName = "EMPLOYEE.ALL" ,ConcurrencyStamp = "23F4A768-BCF3-4BA4-8D20-CC3E4A9C333A"},
+                    new Role { Id = new Guid("29ECF6CE-B82F-4FC5-AE01-08D70CF9F908"), Name = "Employee.Create", Description = "Создание", Category = "Employee", NormalizedName = "EMPLOYEE.CREATE", ConcurrencyStamp = "B8EFD7E4-72E8-4110-96CC-A531AD35D9B4"},
+                    new Role { Id = new Guid("A8EB0E97-EAAA-4976-AE02-08D70CF9F908"), Name = "Employee.Edit", Description = "Редактирование", Category = "Employee", NormalizedName = "EMPLOYEE.EDIT", ConcurrencyStamp = "98003B79-EE18-4D7F-B8A5-357E74E8F77A"},
+                    new Role { Id = new Guid("C5DBEAAB-86A3-4400-B50A-08D70E6B40DC"), Name = "Employee.Details", Description = "Подробная информация", Category = "Employee", NormalizedName = "EMPLOYEE.DETAILS", ConcurrencyStamp = "36271C6B-8972-4A69-90D0-D9921B6F90D3"}
                 );
         }
 
