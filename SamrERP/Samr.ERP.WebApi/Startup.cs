a8f39500a65b1cba83f86626f9d89e808567c6d2
stein <amirashurov11@gmail.com>
2019-08-02 10:32:52 +0500

    cors add

--- SamrERP/Samr.ERP.WebApi/Startup.cs
+++ SamrERP/Samr.ERP.WebApi/Startup.cs
@@ -132,7 +132,9 @@ namespace Samr.ERP.WebApi
                     .AllowAnyHeader()
                     .AllowCredentials()
                     .AllowAnyOrigin()
-                    .WithOrigins("http://localhost:4200", "http://sumr.evomedia.pro", "http://samr.evomedia.pro")
+                    .WithOrigins("http://localhost:4200", 
+                    "http://sumr.evomedia.pro","https://sumr.evomedia.pro",
+                    "http://samr.evomedia.pro","http://samr.evomedia.pro")
                     .WithExposedHeaders("Content-Disposition");
             }));
             services.AddSignalR();
