66129edcd5db355142fc85f3db74002d9bdac445
rahajustone <1992raha1@gmail.com>
2019-08-17 15:17:37 +0500

    Fix Hubs route url

--- SamrERP/Samr.ERP.WebApi/Hub/NotificationHub.cs
+++ SamrERP/Samr.ERP.WebApi/Hub/NotificationHub.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Internal;
 using Microsoft.AspNetCore.SignalR;
 using Samr.ERP.Core.Services;
 using Samr.ERP.Infrastructure.Providers;
@@ -13,14 +14,13 @@ namespace Samr.ERP.WebApi.Hub
 {
     public class NotificationHub : Microsoft.AspNetCore.SignalR.Hub
     {
-        private readonly UserProvider _user;
-
-        public NotificationHub(UserProvider user)
+        public NotificationHub()
         {
-            _user = user;
         }
         public async Task MessageReceived(object user, object message)
         {
+            /*Context*/
+
             await Clients.All.SendAsync("MessageReceived", user, message);
             //await Clients.User(_user.CurrentUser.Id.ToString()).SendAsync("SendMessage", user, message);
         }
