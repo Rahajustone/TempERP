2122279dd9da5283002a05f0d27d9715314d23cc
rahajustone <1992raha1@gmail.com>
2019-08-21 11:23:44 +0500

    Add EmailLogHistory filter

---
+++ SamrERP/Samr.ERP.Core/ViewModels/EmailSetting/EmailMessageHistoryLogFilterViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samr.ERP.Core.ViewModels.EmailSetting
+{
+    public class FilterEmailMessageHistoryLogViewModel
+    {
+        public string ReceiverName { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+    }
+}
