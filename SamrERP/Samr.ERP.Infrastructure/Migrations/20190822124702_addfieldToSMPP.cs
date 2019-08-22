73f6f4a46e03f5f3b63ace593e52a98d5f26da1e
rahajustone <1992raha1@gmail.com>
2019-08-22 17:49:13 +0500

    Add Field to SMPPSetting

---
+++ SamrERP/Samr.ERP.Infrastructure/Migrations/20190822124702_addfieldToSMPP.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Samr.ERP.Infrastructure.Migrations
+{
+    public partial class addfieldToSMPP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProviderName",
+                table: "SMPPSettings",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProviderName",
+                table: "SMPPSettings");
+        }
+    }
+}
